b7083e4dd49f8898b805354c8c0e90afda7092d1
Nathaniel E Adams <47441281+nadamspueblo@users.noreply.github.com>
2026-04-23 22:56:37 +0000

    start

---
+++ script.js
@@ -0,0 +1,11 @@
+let width = 10;
+let height = 10;
+
+let gameboard = [];
+
+for (let r = 0; r < width; r++) {
+    let row = [];
+    for (let c = 0; c < height; c++) {
+        let val = Math.random() * width * height
+    }
+}
